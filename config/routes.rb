147da0e301791500637ec7983107eb899d5af75f
Alysia Brown <alysiabrown@Alysias-MacBook-Pro.local>
2023-03-02 11:12:06 -0800

    Added jobs and checkouts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "/checkout", to: "checkout#index"
+  get "/jobs", to: "jobs#index"
+  # get 'checkout/index'
+  # get 'jobs/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/", to: "home#index"
 end
